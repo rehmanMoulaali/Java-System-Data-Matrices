fbed406cf7a9c42d0cdbfb5dea4e8000b911f3be
rehmanMoulaali <rehman.arjunagi@irisidea.com>
2023-10-09 15:13:01 +0530

    added comments

--- src/SystemMatrices/JVMMemoryMatrix.java
+++ src/SystemMatrices/JVMMemoryMatrix.java
@@ -5,16 +5,16 @@ import java.lang.management.MemoryMXBean;
 
 public class JVMMemoryMatrix {
     private final static MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
-    public static Double getInitialMemory(){
+    public static Double getInitialMemory(){ //initial memory taken by JVM from OS
         return (double)memoryMXBean.getHeapMemoryUsage().getInit() /1073741824;
     }
-    public static Double getUsedMemory(){
+    public static Double getUsedMemory(){ //The heap memory used by program
         return (double)memoryMXBean.getHeapMemoryUsage().getUsed() /1073741824;
     }
-    public static Double getMaxMemory(){
+    public static Double getMaxMemory(){ //the max memory used by the process before hitting overflow error
         return (double)memoryMXBean.getHeapMemoryUsage().getMax() /1073741824;
     }
-    public static Double getCommittedMemory(){
+    public static Double getCommittedMemory(){ // the committed memory by os
         return (double)memoryMXBean.getHeapMemoryUsage().getCommitted() /1073741824;
     }
 
